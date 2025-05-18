756ad2e055a1afd0c7d2c5b1d30112afad6755de
Kinoti Humphrey <humphreykinoti4@gmail.com>
2025-05-18 15:29:29 +0300

    [FIX] added the row number and all ranks

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,7 +9,15 @@ GROUP BY user_id;
 -- Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
 SELECT 
     property_id,
-    COUNT(*) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
-FROM bookings
-GROUP BY property_id;
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_number,
+    RANK() OVER (ORDER BY total_bookings DESC) AS rank,
+    DENSE_RANK() OVER (ORDER BY total_bookings DESC) AS dense_rank
+FROM (
+    SELECT 
+        property_id,
+        COUNT(*) AS total_bookings
+    FROM bookings
+    GROUP BY property_id
+) AS booking_counts;
+
